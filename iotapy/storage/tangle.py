666e71b9c84e6df1f64d34d3cdcce97142617c59
Louie Lu <git@louie.lu>
2017-11-04 14:16:06 +0800

    Add first latest method to tangle

--- iotapy/storage/tangle.py
+++ iotapy/storage/tangle.py
@@ -11,3 +11,9 @@ class Tangle:
 
     def get(self, key, column):
         return self.provider.get(key, column)
+
+    def first(self, column):
+        return self.provider.first(column)
+
+    def latest(self, column):
+        return self.provider.latest(column)
